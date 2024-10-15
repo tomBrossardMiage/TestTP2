585afb496efc05497c0323135b5d422d6636e736
DESKTOP-IOR3PJ0\33778 <tom.brossard@toulouse.miage.fr>
2024-10-15 14:09:13 +0200

    commit initial

---
+++ src/main/java/org/example/Stack.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public interface Stack {
+    /**
+     * Tests if this stack is empty
+     */
+    public boolean isEmpty();
+    /**
+     * Returns the number of items in this stack.
+     */
+    public int getSize(); /**
+     */
+    public void push(double item);
+    /**
+     * Looks at the object at the top of this stack without removing it from the
+     stack.
+     */
+    public double peek() throws EmptyStackException;
+    /**
+     * Removes the object at the top of this stack and returns
+     * that object as the value of this function.
+     * @throws EmptyStackException if this stack is empty.
+     */
+    public double pop() throws EmptyStackException;
+}
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @org.junit.Test
+    public void main() {
+    }
+}
